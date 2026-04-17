0bcae41887638e8872ad6e25c842c78ee25ba5fb
sakibhossaindev <sakibhossain.web.dev@gmail.com>
2026-04-17 22:38:21 +0600

    style:new digine

---
+++ js/javascript.js
@@ -0,0 +1,15 @@
+let headingone = document.querySelector(".headingone");
+let name = document.querySelector(".name");
+let message = document.querySelector("textarea");
+let btnone = document.querySelector(".btnone");
+let cardone = document.querySelector(".cardone");
+
+
+
+
+
+
+
+btnone.addEventListener("click", function(){
+ 
+})
