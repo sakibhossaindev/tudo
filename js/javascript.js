8cf5c4db30e40aa9da96b2364be59d838fd93da2
sakibhossaindev <sakibhossain.web.dev@gmail.com>
2026-05-05 17:17:12 +0600

    new

--- js/javascript.js
+++ js/javascript.js
@@ -1,17 +1,11 @@
-let headingone = document.querySelector(".headingone");
 let name = document.querySelector(".name");
 let message = document.querySelector("textarea");
 let btnone = document.querySelector(".btnone");
 let postone = document.querySelector(".postone");
+let errorone = document.querySelector(".errorone");
+let errortwo = document.querySelector(".errortwo");
 
-let arr = [
-    {
-        name: "sakib",
-        message: "this is a simple caption lo"
-    }
-]
-
-
+let arr = []
 function display() {
     arr.map((item) => {
         postone.innerHTML += `<div class="card-body card-1">
@@ -24,8 +18,28 @@ function display() {
     })
 }
 
-
 btnone.addEventListener("click", () => {
+    errorone.textContent = "";
+    errortwo.textContent = "";
+
+    if (!name.value) {
+        errorone.textContent = "Please enter your name";
+        return;
+    }
+
+    if (!message.value) {
+        errortwo.textContent = "Please enter a caption";
+        return;
+    }
+
+    arr.push({
+        name: name.value,
+        message: message.value
+    })
+    postone.innerHTML = "";
     display();
+    name.value = "";
+    message.value = "";
+
 })
 
